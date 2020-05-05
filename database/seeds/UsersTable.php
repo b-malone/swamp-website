27d27024ad0b53e960c6c5c4d09e169fef5f270f
Bailey Malone <bmalone@pndlm.com>
2020-05-05 16:00:27 -0700

    Removing unnecessary seeds.

--- database/seeds/UsersTable.php
+++ database/seeds/UsersTable.php
@@ -20,44 +20,6 @@ class UsersTable extends Seeder
       // ]);
 
       // CREATE users from development
-      DB::table('users')->insert([
-          'id' => 2,
-          'role_id' => 1,
-          'name' => 'peter ritsen',
-          'password' => bcrypt('frog12'),
-          'email' => 'clarkfrog@gmail.com',
-          'avatar' => 'users/default.png',
-          'remember_token' => NULL,
-          'settings' => NULL,
-          'created_at' => Carbon::now(),
-          'updated_at' => Carbon::now()
-      ]);
-
-      DB::table('users')->insert([
-          'id' => 3,
-          'role_id' => 1,
-          'name' => 'bmalone',
-          'password' => bcrypt('bmalone'),
-          'email' => 'bmalone.developer@gmail.com',
-          'avatar' => 'users/default.png',
-          'remember_token' => NULL,
-          'settings' => NULL,
-          'created_at' => Carbon::now(),
-          'updated_at' => Carbon::now()
-      ]);
-
-      DB::table('users')->insert([
-          'id' => 4,
-          'role_id' => 1,
-          'name' => 'michelle devlaeminck',
-          'password' => bcrypt('b@r3f33t'),
-          'email' => 'madevlaeminck@gmail.com',
-          'avatar' => 'users/default.png',
-          'remember_token' => NULL,
-          'settings' => NULL,
-          'created_at' => Carbon::now(),
-          'updated_at' => Carbon::now()
-      ]);
 
       // SETUP User-Role Relations for existing users
       $this->call([
