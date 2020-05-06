0febba299ccd6211550d6868bf9ff83d3ad86872
Bailey Malone <bmalone@pndlm.com>
2020-05-05 17:34:49 -0700

    Removing debug users. Admin users require SSH and artisan command run, see Voyager docs.

--- database/seeds/UserRolesTable.php
+++ database/seeds/UserRolesTable.php
@@ -1,29 +1,17 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class UserRolesTable extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        DB::table('user_roles')->insert([
-          'user_id' => 2,
-          'role_id' => 1
-        ]);
+		/**
+		 * Run the database seeds.
+		 *
+		 * @return void
+		 */
+		public function run()
+		{
 
-        DB::table('user_roles')->insert([
-          'user_id' => 3,
-          'role_id' => 1
-        ]);
-
-        DB::table('user_roles')->insert([
-          'user_id' => 4,
-          'role_id' => 1
-        ]);
-    }
+		}
 }
--- database/seeds/UsersTable.php
+++ database/seeds/UsersTable.php
@@ -20,31 +20,31 @@ class UsersTable extends Seeder
       // ]);
 
       // CREATE users from development
-      DB::table('users')->insert([
-          'id' => 2,
-          'role_id' => 1,
-          'name' => 'peter ritsen',
-          'password' => bcrypt('frog12'),
-          'email' => 'clarkfrog@gmail.com',
-          'avatar' => 'users/default.png',
-          'remember_token' => NULL,
-          'settings' => NULL,
-          'created_at' => Carbon::now(),
-          'updated_at' => Carbon::now()
-      ]);
+      // DB::table('users')->insert([
+      //     'id' => 2,
+      //     'role_id' => 1,
+      //     'name' => 'peter ritsen',
+      //     'password' => bcrypt('frog12'),
+      //     'email' => 'clarkfrog@gmail.com',
+      //     'avatar' => 'users/default.png',
+      //     'remember_token' => NULL,
+      //     'settings' => NULL,
+      //     'created_at' => Carbon::now(),
+      //     'updated_at' => Carbon::now()
+      // ]);
 
-      DB::table('users')->insert([
-          'id' => 4,
-          'role_id' => 1,
-          'name' => 'michelle devlaeminck',
-          'password' => bcrypt('b@r3f33t'),
-          'email' => 'madevlaeminck@gmail.com',
-          'avatar' => 'users/default.png',
-          'remember_token' => NULL,
-          'settings' => NULL,
-          'created_at' => Carbon::now(),
-          'updated_at' => Carbon::now()
-      ]);
+      // DB::table('users')->insert([
+      //     'id' => 4,
+      //     'role_id' => 1,
+      //     'name' => 'michelle devlaeminck',
+      //     'password' => bcrypt('b@r3f33t'),
+      //     'email' => 'madevlaeminck@gmail.com',
+      //     'avatar' => 'users/default.png',
+      //     'remember_token' => NULL,
+      //     'settings' => NULL,
+      //     'created_at' => Carbon::now(),
+      //     'updated_at' => Carbon::now()
+      // ]);
 
       // SETUP User-Role Relations for existing users
       $this->call([
